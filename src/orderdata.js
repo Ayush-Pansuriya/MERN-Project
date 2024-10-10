06f2f510886c2e1a01575e735d9d7b037b3bc8b0
Ayush-Pansuriya <23031701037@darshan.ac.in>
2024-10-10 13:29:33 +0530

    Add files via upload

---
+++ src/orderdata.js
@@ -0,0 +1,18 @@
+export  const data=[
+    {
+        id:1,
+        cfullname:Ayush,
+        pname:soap,
+        price:100,
+        quantity:10,
+        total:price*quantity,
+        },
+        {
+            id:2,
+            cfullname:Rohan,
+            pname:shampoo,
+            price:80,
+            quantity:4,
+            total:price*quantity,
+    }
+]
